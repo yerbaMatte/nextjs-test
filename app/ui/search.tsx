02709bdee85d3e5cdd4c216d9d2e4f600e8c3d88
yerbaMatte <milosz.lewandowski@icloud.com>
2024-03-11 23:31:16 +0100

    query params and pagination SSR

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,18 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useEffect, useRef } from 'react';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  let inputElement;
 
-  function handleChange(term: string) {
+  function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+
     if (term) {
       params.set('query', term);
     } else {
@@ -17,16 +21,25 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     router.replace(`${pathname}?${params.toString()}`);
   }
+
+  useEffect(() => {
+    inputElement = inputRef.current;
+    console.log(inputElement?.value);
+  }, []);
+  //
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        ref={inputRef}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => handleChange(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
+
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
